c0ea90cbce9204f54825c332154503bc884f52f7
bijanbina <bijanbina@gmail.com>
2022-07-10 04:12:00 +0200

    MFST: Minimalize More The FST Lib, Add Binary for Win64

--- mfst/MFST.pro
+++ mfst/MFST.pro
@@ -3,13 +3,9 @@ QT -= gui
 TEMPLATE = lib
 CONFIG += staticlib
 
-INCLUDEPATH += ../PNN
+INCLUDEPATH += ../
 
 CONFIG += c++11
-
-# You can make your code fail to compile if it uses deprecated APIs.
-# In order to do so, uncomment the following line.
-#DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
 SOURCES += \
     Sources/compat.cc \
--- mfst/Sources/fst-types.cc
+++ mfst/Sources/fst-types.cc
@@ -9,36 +9,36 @@
 
 namespace fst {
 
-REGISTER_FST(VectorFst, StdArc);
-REGISTER_FST(VectorFst, LogArc);
-REGISTER_FST(VectorFst, Log64Arc);
+//REGISTER_FST(VectorFst, StdArc);
+//REGISTER_FST(VectorFst, LogArc);
+//REGISTER_FST(VectorFst, Log64Arc);
 
-REGISTER_FST(ConstFst, StdArc);
-REGISTER_FST(ConstFst, LogArc);
-REGISTER_FST(ConstFst, Log64Arc);
+//REGISTER_FST(ConstFst, StdArc);
+//REGISTER_FST(ConstFst, LogArc);
+//REGISTER_FST(ConstFst, Log64Arc);
 
-REGISTER_FST(EditFst, StdArc);
-REGISTER_FST(EditFst, LogArc);
-REGISTER_FST(EditFst, Log64Arc);
+//REGISTER_FST(EditFst, StdArc);
+//REGISTER_FST(EditFst, LogArc);
+//REGISTER_FST(EditFst, Log64Arc);
 
-REGISTER_FST(CompactStringFst, StdArc);
-REGISTER_FST(CompactStringFst, LogArc);
-REGISTER_FST(CompactStringFst, Log64Arc);
+//REGISTER_FST(CompactStringFst, StdArc);
+//REGISTER_FST(CompactStringFst, LogArc);
+//REGISTER_FST(CompactStringFst, Log64Arc);
 
-REGISTER_FST(CompactWeightedStringFst, StdArc);
-REGISTER_FST(CompactWeightedStringFst, LogArc);
-REGISTER_FST(CompactWeightedStringFst, Log64Arc);
+//REGISTER_FST(CompactWeightedStringFst, StdArc);
+//REGISTER_FST(CompactWeightedStringFst, LogArc);
+//REGISTER_FST(CompactWeightedStringFst, Log64Arc);
 
-REGISTER_FST(CompactAcceptorFst, StdArc);
-REGISTER_FST(CompactAcceptorFst, LogArc);
-REGISTER_FST(CompactAcceptorFst, Log64Arc);
+//REGISTER_FST(CompactAcceptorFst, StdArc);
+//REGISTER_FST(CompactAcceptorFst, LogArc);
+//REGISTER_FST(CompactAcceptorFst, Log64Arc);
 
-REGISTER_FST(CompactUnweightedFst, StdArc);
-REGISTER_FST(CompactUnweightedFst, LogArc);
-REGISTER_FST(CompactUnweightedFst, Log64Arc);
+//REGISTER_FST(CompactUnweightedFst, StdArc);
+//REGISTER_FST(CompactUnweightedFst, LogArc);
+//REGISTER_FST(CompactUnweightedFst, Log64Arc);
 
-REGISTER_FST(CompactUnweightedAcceptorFst, StdArc);
-REGISTER_FST(CompactUnweightedAcceptorFst, LogArc);
-REGISTER_FST(CompactUnweightedAcceptorFst, Log64Arc);
+//REGISTER_FST(CompactUnweightedAcceptorFst, StdArc);
+//REGISTER_FST(CompactUnweightedAcceptorFst, LogArc);
+//REGISTER_FST(CompactUnweightedAcceptorFst, Log64Arc);
 
 }  // namespace fst
