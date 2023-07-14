488ff4e8df60723c37a617b28d2a5ce2be471edf
bijanbina <bijanbina@gmail.com>
2023-07-14 09:14:20 +0200

    Sag: Successful DLL Injection

--- sag/Sources/main.cpp
+++ sag/Sources/main.cpp
@@ -13,80 +13,27 @@ static HINSTANCE hInstance;
 static HWND hGDWindow;
 static HHOOK hKbdHook;
 
-__declspec (dllexport) void removeHook()
+BOOL APIENTRY DllMain (HINSTANCE hndl, DWORD reason,
+                       LPVOID reserved)
 {
-    if( hKbdHook )
+    (void) reserved; // to avoid not used warning
+    if( reason==DLL_PROCESS_ATTACH )
     {
-        UnhookWindowsHookEx( hKbdHook );
-        hKbdHook = 0;
+        hInstance = hndl;
+        MessageBox(0, L"Hello From testlib!", L"Hello", MB_ICONINFORMATION);
     }
-}
-
-__declspec (dllexport) BOOL setHook( HWND hwnd )
-{
-    hGDWindow = hwnd;
-    removeHook();
-    hKbdHook = SetWindowsHookEx( WH_KEYBOARD_LL, lowLevelKeyboardProc, hInstance, 0 );
-    return hKbdHook != 0;
-}
-
-__declspec (dllexport) void clearHotkeys()
-{
+    else if( reason==DLL_PROCESS_DETACH )
+    {
+        ;// Try to constructor here
+    }
+    else if( reason==DLL_THREAD_ATTACH )
+    {
         ;
-}
-
-static BOOL isModifiersPressed( DWORD modifiers )
-{
-    int n = GetAsyncKeyState( VK_MENU ) & 0x8000;
-    if( ( ( modifiers & MOD_ALT ) && n == 0 )
-            || ( ( modifiers & MOD_ALT ) == 0 && n ) )
-        return FALSE;
-
-    n = GetAsyncKeyState( VK_SHIFT ) & 0x8000;
-    if( ( ( modifiers & MOD_SHIFT ) && n == 0 )
-            || ( ( modifiers & MOD_SHIFT ) == 0 && n ) )
-        return FALSE;
-
-    n = GetAsyncKeyState( VK_CONTROL ) & 0x8000;
-    if( ( ( modifiers & MOD_CONTROL ) && n == 0 )
-            || ( ( modifiers & MOD_CONTROL ) == 0 && n ) )
-        return FALSE;
-
-    n = ( GetAsyncKeyState( VK_LWIN ) & 0x8000 ) | ( GetAsyncKeyState( VK_RWIN ) & 0x8000 );
-    if( ( ( modifiers & MOD_WIN ) && n == 0 )
-            || ( ( modifiers & MOD_WIN ) == 0 && n ) )
-        return FALSE;
-
-    return TRUE;
-}
-
-static LRESULT CALLBACK lowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
-{
-
-}
-
-BOOL APIENTRY DllMain (HINSTANCE hInst     /* Library instance handle. */ ,
-                       DWORD reason        /* Reason this function is being called. */ ,
-                       LPVOID reserved     /* Not used. */ )
-{
-    (void) reserved;
-    switch (reason)
-    {
-    case DLL_PROCESS_ATTACH:
-        hInstance = hInst;
-        break;
-
-    case DLL_PROCESS_DETACH:
-        removeHook();
-        break;
-
-    case DLL_THREAD_ATTACH:
-        break;
-
-    case DLL_THREAD_DETACH:
-        break;
     }
-
+    else if( reason==DLL_THREAD_ATTACH )
+    {
+        ;
+    }
     /* Returns TRUE on success, FALSE on failure */
     return TRUE;
 }
