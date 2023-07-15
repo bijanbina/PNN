8625818b6ed6eeab32dc25ca3cf5accfb7ba577a
bijanbina <bijanbina@gmail.com>
2023-07-15 16:23:56 +0200

    Sag: Add Couple of MessageBox

--- sag/Sources/main.cpp
+++ sag/Sources/main.cpp
@@ -1,5 +1,7 @@
 #include "hook.h"
 #include <stdio.h>
+#include <QString>
+#include <iostream>
 
 static LRESULT CALLBACK lowLevelKeyboardProc(int, WPARAM, LPARAM);
 
@@ -12,28 +14,64 @@ static LRESULT CALLBACK lowLevelKeyboardProc(int, WPARAM, LPARAM);
 static HINSTANCE hInstance;
 static HWND hGDWindow;
 static HHOOK hKbdHook;
+HMODULE handle;
+
+void showMessage(const WCHAR *txt);
+DWORD mainThread(HMODULE mod_h);
 
 BOOL APIENTRY DllMain (HINSTANCE hndl, DWORD reason,
                        LPVOID reserved)
 {
     (void) reserved; // to avoid not used warning
+
     if( reason==DLL_PROCESS_ATTACH )
     {
         hInstance = hndl;
-        MessageBox(0, L"Hello From testlib!", L"Hello", MB_ICONINFORMATION);
+//        MessageBox(0, L"Hell!", L"Hello", MB_ICONINFORMATION);
+        showMessage(L"KOKKO");
+        LPTHREAD_START_ROUTINE main_th =
+                (LPTHREAD_START_ROUTINE)mainThread;
+        HANDLE th = CreateThread(NULL, 0, main_th, hndl, 0, NULL);
+//        CloseHandle(th);
     }
     else if( reason==DLL_PROCESS_DETACH )
     {
-        ;// Try to constructor here
+        MessageBox(0, L"Detach Lib!", L"Hello", MB_ICONINFORMATION);
+        ;// Run deconstructor here
     }
     else if( reason==DLL_THREAD_ATTACH )
     {
-        ;
+        MessageBox(0, L"Attach !", L"Hello", MB_ICONINFORMATION);
     }
-    else if( reason==DLL_THREAD_ATTACH )
+    else if( reason==DLL_THREAD_DETACH )
     {
-        ;
+        MessageBox(0, L"Detach Thread!", L"Hello", MB_ICONINFORMATION);
+        handle = GetModuleHandle(L"Sag.dll");
+        FreeLibrary(handle);
+    }
+    else
+    {
+        MessageBox(0, L"Fuck!", L"Hello", MB_ICONINFORMATION);
     }
     /* Returns TRUE on success, FALSE on failure */
     return TRUE;
+}
+
+DWORD mainThread(HMODULE mod_h)
+{
+    (void) mod_h;
+//    AllocConsole();
+//    FILE *f = new FILE();
+//    freopen_s(&f, "CONOUT$", "w", stdout);
+
+//    std::cout << "sag tu in zendegi" << std::endl;
+
+//    std::cout << "sag tu in zendegi" << handle << std::endl;
+    return 0;
+}
+
+void showMessage(const WCHAR *txt)
+{
+    MessageBox(0, txt, L"DLL",
+               MB_ICONINFORMATION);
 }
